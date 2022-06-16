26469d48d5f85bb70e3539593f17dbcec6da2f84
MaxBaldus <maxbaldus@gmx.net>
2022-06-16 19:44:54 +0200

    structure setup

---
+++ 00_Main.R
@@ -0,0 +1,6 @@
+rm(list=ls()) # clear out all variables in current session 
+
+source("01_load_packages.R")  # load all neceassry packes
+
+
+
---
+++ 01_load_packages.R
@@ -0,0 +1,11 @@
+a = 0
+if(a == 1){
+  install.packages("randomForest")
+}
+
+
+# load packages
+library(keras)
+
+plot(cars)
+
